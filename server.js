f9cc29aa2baa48be311dbe7824886e26fe986cdf
pozieblo <pozieblo@gmail.com>
2023-11-03 10:58:43 +0100

    Slow server functionality

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 
 // Require the http module
 const http = require('http');
+require('dotenv').config();
+const timeout = process.env.MY_TIMEOUT;
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
    
 // Create a server object
 const server = http.createServer((req, res) => {
-  // Set the response status code to 500 (Internal Server Error)
-  res.statusCode = 500;
   // Set the response header content type to text/plain
   res.setHeader('Content-Type', 'text/plain');
-  // Write a message to the response body
-  res.end('Something went wrong!');
+  // Set a timeout function to delay the response
+  setTimeout(() => {
+      // Write the response body
+      res.write('Hello World');
+      // End the response
+      res.end();
+    }, MY_TIMEOUT);
+    
   console.log('Someone tried to call url: ' + req.url);
 });
 
